004026213c1edeaf673dab4c7b2a185ed730a1d6
inkirby <inkirby@gmail.com>
2026-08-10 16:45:42 +0700

    Initial Commit

---
+++ ControlWake/ControlWakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ControlWakeApp.swift
+//  ControlWake
+//
+//  Created by Chanthat Onsawang on 10/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct ControlWakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
